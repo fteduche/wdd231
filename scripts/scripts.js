c78c6b5251a6f136c74b304ab58869b5978547e6
Fred Frank U <fteduche@gmail.com>
2025-03-07 17:47:50 +0100

    wdd231 assigment

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+// Get the current year and last modified date
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+const lastModified = document.lastModified;
+
+// Update the footer with the current year and last modified date
+document.getElementById("last-modified").textContent = lastModified;
