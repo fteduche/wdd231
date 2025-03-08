e6f4ef2a1d58e88d283e340d944596d0347ea13f
Fred Frank U <fteduche@gmail.com>
2025-03-08 13:14:08 +0100

    Second commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,4 +4,4 @@ const year = currentDate.getFullYear();
 const lastModified = document.lastModified;
 
 // Update the footer with the current year and last modified date
-document.getElementById("last-modified").textContent = lastModified;
+document.getElementById("last-modified").textContent = "Last Update "+lastModified;
