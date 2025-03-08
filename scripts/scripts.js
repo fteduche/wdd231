3262a2223184c9e03d1d2520fd5780587621f8d6
Fred Frank U <fteduche@gmail.com>
2025-03-08 14:37:01 +0100

    8th commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,35 +1,51 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.getElementById('hamburger');
     const menu = document.getElementById('menu');
-
     hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('active');
-        menu.classList.toggle('active');
+      hamburger.classList.toggle('active');
+      menu.classList.toggle('active');
+      const nav = document.querySelector("nav ul");
+      nav.classList.toggle("show");
     });
-});
-
-// Call functions on page load
-window.onload = () => {
+  });
+  
+  // Call functions on page load
+  window.onload = () => {
     displayFooterInfo();
   };
-
-
-// Add event listener to the hamburger menu button
-document.getElementById("hamburger-menu").addEventListener("click", () => {
-    // Toggle the navigation menu
-    const nav = document.querySelector("nav ul");
-    nav.classList.toggle("show");
-});
-
-// Add event listener to the window to toggle the navigation menu on resize
-window.addEventListener("resize", () => {
-    const nav = document.querySelector("nav ul")});
-
-
-// Get the current year and last modified date
-const currentDate = new Date();
-const year = currentDate.getFullYear();
-const lastModified = document.lastModified;
-
-// Update the footer with the current year and last modified date
-document.getElementById("last-modified").textContent = "Last Update "+lastModified;
+  
+  // Function for filter
+  const allBtn = document.getElementById('all-btn');
+  const cseBtn = document.getElementById('cse-btn');
+  const wddBtn = document.getElementById('wdd-btn');
+  const items = document.querySelectorAll('.grid-container div');
+  
+  allBtn.addEventListener('click', () => {
+    items.forEach(item => item.style.display = 'block');
+  });
+  
+  cseBtn.addEventListener('click', () => {
+    items.forEach(item => {
+      if (item.classList.contains('cse')) {
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  });
+  
+  wddBtn.addEventListener('click', () => {
+    items.forEach(item => {
+      if (item.classList.contains('wdd')) {
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  });
+  
+  // Get the current year and last modified date
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  // Update the footer with the current year
+  document.getElementById("last-modified").textContent = "  " + year;
