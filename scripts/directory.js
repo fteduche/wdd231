03b1ec500ee77c51331605b4a21fbe03ff526529
Fred Frank U <fteduche@gmail.com>
2025-03-21 12:32:51 +0100

    correction to display last modified date

--- scripts/directory.js
+++ scripts/directory.js
@@ -58,3 +58,11 @@ async function loadJSONData() {
 
 // Call the async function
 loadJSONData();
+
+// Get the current year and last modified date
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+const lastModified = document.lastModified;
+
+// Update the footer with the current year and last modified date
+document.getElementById("last-modified").textContent = "Last Update: " + lastModified;
